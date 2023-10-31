007c9446958889cc0ddf458b4f3d7ac80f691ed1
Evandro Santana <evandrosant@gmail.com>
2023-10-31 16:24:27 -0300

    label

--- react/components/SkuId.tsx
+++ react/components/SkuId.tsx
@@ -5,15 +5,20 @@ import { Item } from '../typings'
 
 // const CSS_HANDLES = ['skuName'] as const
 
-// interface Props {
-//   showLabel: boolean
-// }
+interface Props {
+  showLabel: boolean
+}
 
-const SkuId = () => {
+const SkuId = ({ showLabel }: Props) => {
   const { sku }: { sku: Item } = useSku()
   // const handles = useCssHandles(CSS_HANDLES)
   return (
     <div>
+      {showLabel && (
+        <span className="t-body c-on-base fw7 pr3">
+          <FormattedMessage id="store/sku-list.sku.price.id" />:{' '}
+        </span>
+      )}
       <span className="c-muted-1">{sku.itemId}</span>
     </div>
   )
